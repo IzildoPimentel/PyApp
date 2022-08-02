c470cab8bfe0aea9551fc1100b93e65dbec92258
Izildo Pimentel <>
2022-08-02 21:05:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+msg = 'I go to starbucks'
+
+new_msg = msg[3] + msg[14] + msg[5] + msg[3]
+
+print(new_msg)
+print(new_msg.upper())
+print(new_msg.lower())
+print(new_msg.capitalize())
+print(new_msg.title())
